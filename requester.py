5701ea94f3a968b0c0423634599c1ea34c5c3576
EugeneDar <evgenedar@mail.ru>
2022-08-27 15:05:06 +0300

    token not visible now

--- requester.py
+++ requester.py
@@ -2,26 +2,13 @@ import time
 
 import requests
 
-token = "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"
-
-# search_final_url = 'https://api.github.com/search/code?q=something+in:file+language:java'
-#
-# url2 = 'https://api.github.com/search/code?q=endsWith'
-#
-# url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-# response = requests.get(url2, verify='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')
-# # print("Status code: ", response.status_code)
-# response_dict = response.json()
-# # print(response_dict.keys())
-# # print(response_dict['total_count'])
-# print(response_dict)
-
+token = ""
 url = "https://api.github.com/search/code?q=addClass+in:file"
 
 url2 = "https://api.github.com/search/code?q=endsWith+in:file"
 
 headers = {
-  'Authorization': 'Token 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'
+  'Authorization': ('Token ' + token)
 }
 
 response = requests.request("GET", url, headers=headers).json()
@@ -30,7 +17,9 @@ print(response)
 print(response['total_count'])
 print()
 
+print('start sleep')
 time.sleep(10)
+print('wake up')
 
 response2 = requests.request("GET", url2, headers=headers).json()
 
